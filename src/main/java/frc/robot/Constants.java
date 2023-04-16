12202a5de7d503ce4893f175730fcbc73d395bb2
anonimous-user <harrisbond1234@gmail.com>
2023-04-15 23:51:35 -0400

    first commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,215 @@
+package frc.robot;
+
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.MecanumDriveKinematics;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+    public static final class OIConstants {
+        // Controller for driving
+        public static final class DriverControl {
+            public static final int kDriverControllerPort = 0;
+            public static final double kDriveDeadband = 0.2;
+            public static final double kRotationDeadband = 0.1;
+            public static final double kZeroCalibrateLeftY = 0.0;
+            public static final double kZeroCalibrateRightX = 0.0;
+            public static final double kZeroCalibrateLeftX = 0.0;
+        }
+
+        // Controller for operating intake
+        public static final class OperatorControl {
+            public static final int kOperatorControllerPort = 1;
+            public static final double kZeroCalibrateLeftY = 0.0;
+            public static final double kZeroCalibrateRightX = 0.0;
+        }
+    }
+
+    public static final class DriveConstants {
+        // Motor ports
+        public static final int kRearRightMotorPort = 1;
+        public static final int kFrontRightMotorPort = 2;
+        public static final int kFrontLeftMotorPort = 3;
+        public static final int kRearLeftMotorPort = 4;
+
+        // Motor inversions
+        public static final boolean kFrontLeftInverted = false;
+        public static final boolean kFrontRightInverted = true;
+        public static final boolean kRearLeftInverted = false;
+        public static final boolean kRearRightInverted = true;
+
+        // Misc motor constants
+        public static final int kSmartCurrentLimitAmps = 40;
+
+        // Distance between centers of right and left wheels on robot
+        public static final double kTrackWidth = Units.inchesToMeters(23.0);;
+        // Distance between centers of front and back wheels on robot
+        public static final double kWheelBase = Units.inchesToMeters(20.50);
+
+        // Kinematics
+        public static final MecanumDriveKinematics kDriveKinematics =
+            new MecanumDriveKinematics(
+                new Translation2d(kWheelBase / 2, kTrackWidth / 2),
+                new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
+                new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
+                new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
+
+        // Using tough box micros installed with the standard gear ratio of 8.45 : 1
+        public static final double kGearRatio = 8.45; // For every 8.45 encoder rotations, 1 wheel rotation
+        public static final double kWheelRadiusMeters = Units.inchesToMeters(8.0 / 2.0); // 8" diameter wheels
+        public static final double kRotationsToMeterConversionFactor = (1.0 / kGearRatio) * 2.0 * Math.PI * kWheelRadiusMeters;
+        public static final double kRpmToMeterPerSecondConversionFactor = kRotationsToMeterConversionFactor / 60.0;
+
+        // These are example values only - DO NOT USE THESE FOR YOUR OWN ROBOT!
+        // These characterization values MUST be determined either experimentally or theoretically
+        // for *your* robot's drive.
+        // #TODO The SysId tool provides a convenient method for obtaining these values for your robot.
+        public static final SimpleMotorFeedforward kFeedforward =
+            new SimpleMotorFeedforward(1, 0.8, 0.15);
+
+        // #TODO Example value only - as above, this must be tuned for your drive!
+        public static final double kPFrontLeftVel = 0.5;
+        public static final double kPRearLeftVel = 0.5;
+        public static final double kPFrontRightVel = 0.5;
+        public static final double kPRearRightVel = 0.5;
+    }
+
+    public static final class AutoDriveConstants {
+        public static final double kMaxSpeedMetersPerSecond = 3;
+        public static final double kMaxAccelerationMetersPerSecondSquared = 3;
+        public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
+        public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
+
+        public static final double kPXController = 0.5;
+        public static final double kPYController = 0.5;
+        public static final double kPThetaController = 0.5;
+
+        // Constraint for the motion profilied robot angle controller
+        public static final TrapezoidProfile.Constraints kThetaControllerConstraints =
+            new TrapezoidProfile.Constraints(
+                kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
+    }
+
+    public static final class ElevatorSubsystemConstants {
+        // Motor constants
+        public static final int kMotorPort = 5;
+        public static final int kSmartCurrentLimit = 20;
+
+        // Encoder constants
+        public static final double kDistancePerRevolution = Units.inchesToMeters(28.0/7.0); // measured 7 rotations = 28"
+        public static final double kVelocityMetersPerSecond = kDistancePerRevolution/60.0;
+    
+        // Soft limit constants
+        public static final double kMinHeight = 0.1;
+        public static final double kMaxHeight = 9.5;
+    
+        // PID controller constants
+        public static final double kP = 1.0;
+        public static final double kI = 0.0;
+        public static final double kD = 0.0;
+        public static final double kIZone = 0.0;
+        public static final double kFeedForwardVelocity = 0.3149; // input to keep it from falling
+    
+        // Controller constants
+        public static final double kMaxControllerUpSpeed = 0.6;
+        public static final double kMaxControllerDownSpeed = -0.6;
+        public static final double kControllerDeadband = 0.1;
+    
+        // Field heights constants
+        public static final double kTopLayerHeight = 7.45;
+        public static final double kMidLayerHeight = 5.74;
+        public static final double kBottomLayerHeight = 0.1;;
+
+    }
+
+    public static final class ArmSubsystemConstants {
+        // Motor constants
+        public static final int kMotorPort = 6;
+        public static final int kSmartCurrentLimit = 20;
+
+        // Encoder constants
+        public static final double kDistancePerRevolution = Units.inchesToMeters(28.0/7.0);
+        public static final double kVelocityMetersPerSecond = kDistancePerRevolution/60.0;
+
+        // Soft limit constants
+        public static final double kMinExtend = 0.0;
+        public static final double kMaxExtend = 11;
+    
+        // PID controller constants
+        public static final double kP = 0.1;
+        public static final double kI = 0.0;
+        public static final double kD = 0.0;
+        public static final double kIZone = 0.0;
+        public static final double kFeedForwardVelocity = 0;
+    
+        // Controller constants
+        public static final double kMaxControllerUpSpeed = 0.6;
+        public static final double kMaxControllerDownSpeed = -0.6;
+        public static final double kControllerDeadband = 0.1;
+    }
+    
+    public static final class PivotSubsystem {
+        // Motor constants
+        public static final int kMotorPort = 7;
+        public static final int kSmartCurrentLimit = 80;
+
+        // Encoder constants
+        public static final double kDistancePerRevolution = Units.inchesToMeters(28.0/7.0);
+        public static final double kVelocityMetersPerSecond = kDistancePerRevolution/60.0;
+    
+        // Soft limit constants
+        public static final double kMinOut = 0.1;
+        public static final double kMaxOut = 3.5;
+    
+        // PID controller constants
+        public static final double kP = 0.1;
+        public static final double kI = 0.0;
+        public static final double kD = 0.0;
+        public static final double kIZone = 0.0;
+        public static final double kFeedForwardVelocity = 0.5;
+    
+        // Controller constants
+        public static final double kMaxControllerUpSpeed = 0.5;
+        public static final double kMaxControllerDownSpeed = -0.5;
+        public static final double kControllerDeadband = 0.1;
+  }
+
+    public static final class ClawSubsystemConstants {
+    public static final int kForwardChannel = 15;
+    public static final int kReverseChannel = 0;
+    public static final int kRMotorPort = 9;
+    public static final int kLMotorPort = 8;
+  }
+
+    public static final class LimelightSubsystem {
+        // Limelight configuration: http://10.49.51.11:5801/
+
+        // Limelight names
+        public static String kLimelightName = "limelight"; // Default name, may change to support multiple limelight
+
+        // Pipelines
+        public static int kAprilTagsWatchPosePipelineID = 0;
+        public static int kRetroTapePostAlignmentPipelineID = 1;
+        public static int kAprilTagsPostAlignmentPipelineID = 2;
+    }
+
+    public static final class Vision {
+        public static double kAlignmentKP = 0.05; // P factor to use for auto alignment routines
+        public static double kSpinClockwiseSearchRate = 0.05; // Missing target spin rate for search during alignment
+    }
+
+    public static final class BalancePID {
+        public static final double BalanceKp = 0.5;
+        public static final double BalanceKi = 0.0;
+        public static final double BalanceKd = 0.05;
+    }
+}
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,170 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.XboxController.Button;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+
+import frc.robot.Subsystems.*;
+import frc.robot.Commands.*;
+
+/*
+ * This class is where the bulk of the robot should be declared.  Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
+ * (including subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+    // Declare robot subsystems
+    public final Subsys_Drive m_robotDrive = new Subsys_Drive();
+    private final Subsys_Elevator m_elevator = new Subsys_Elevator();
+    private final Subsys_Arm m_arm = new Subsys_Arm();
+    private final Subsys_Pivot m_pivot = new Subsys_Pivot();
+    private final Subsys_Claw m_reach = new Subsys_Claw();
+    private final Subsys_Intake m_claw = new Subsys_Intake();
+    // private final LimelightSubsystem m_limeLight = new LimelightSubsystem(Constants.LimelightSubsystem.kLimelightName);
+
+    // Declare input controllers
+    XboxController m_driverController = new XboxController(Constants.OIConstants.DriverControl.kDriverControllerPort);
+    XboxController m_operatorController = new XboxController(Constants.OIConstants.OperatorControl.kOperatorControllerPort);
+
+    /**
+     * The container for the robot. Contains subsystems, OI devices, and commands.
+     */
+    public RobotContainer() {
+        // Configure default commands
+        configureDefaultCommands();
+
+        // Configure button bindings
+        configureButtonBindings();
+    }
+
+    /**
+     * Set default command for robot's subsystems.
+     */
+    private void configureDefaultCommands() {
+        // Input operated sriving method
+        m_robotDrive.setDefaultCommand(new InstantCommand(()-> {
+                    var controllerLeftX = m_driverController.getLeftX();
+                    var controllerLeftY = m_driverController.getLeftY();
+                    var controllerRightX = m_driverController.getRightX();
+
+                    m_robotDrive.driveMecanum(
+                        MathUtil.applyDeadband(controllerLeftX,
+                            Constants.OIConstants.DriverControl.kDriveDeadband),
+                        MathUtil.applyDeadband(-controllerLeftY,
+                            Constants.OIConstants.DriverControl.kDriveDeadband),
+                        MathUtil.applyDeadband(controllerRightX * 0.67,
+                            Constants.OIConstants.DriverControl.kRotationDeadband)
+                    );
+                }, m_robotDrive));
+        
+        // Pivot set to constant speed if input provided
+        m_pivot.setDefaultCommand(new RunCommand(()-> {
+                var controllerLeftY = m_operatorController.getLeftY() +
+                    Constants.OIConstants.OperatorControl.kZeroCalibrateLeftY;
+                if (Math.abs(controllerLeftY) > 0.2) {
+                    m_pivot.setSpeed((Math.abs(controllerLeftY) / -controllerLeftY) * 0.2);
+                } else {
+                    m_pivot.setSpeed(0);
+                }
+            }, m_pivot));
+        
+        // Arm set to constant speed if input provided
+        m_arm.setDefaultCommand(new RunCommand(()-> {
+                var controllerRightY = m_operatorController.getRightY();
+                if (Math.abs(controllerRightY) > 0.2) {
+                    m_arm.setSpeed((Math.abs(controllerRightY) / -controllerRightY) * 0.4);
+                } else {
+                    m_arm.setSpeed(0);
+                }
+            }, m_arm));
+    }
+
+    /**
+     * Use this method to define your button->command mappings. Buttons can be
+     * created by
+     * instantiating a {@link edu.wpi.first.wpilibj.GenericHID} or one of its
+     * subclasses ({@link
+     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then calling
+     * passing it to a
+     * {@link JoystickButton}.
+     */
+    private void configureButtonBindings() {
+        // Elevator high position
+        new JoystickButton(m_operatorController, Button.kY.value)
+            .onTrue(new InstantCommand(()-> m_elevator.setPosition(Constants.ElevatorSubsystemConstants.kTopLayerHeight)));
+
+        // Elevator mid position
+        new JoystickButton(m_operatorController, Button.kB.value)
+            .onTrue(new InstantCommand(()-> m_elevator.setPosition(Constants.ElevatorSubsystemConstants.kMidLayerHeight)));
+
+        // Elevator low position
+        new JoystickButton(m_operatorController, Button.kA.value)
+            .onTrue(new InstantCommand(()-> m_elevator.setPosition(Constants.ElevatorSubsystemConstants.kBottomLayerHeight)));
+
+        // Pivot position closed
+        new JoystickButton(m_operatorController, Button.kX.value)
+            .onTrue(new CmdHybridManip_PivotGoToPosition(m_pivot, 0.1));
+
+        // Pivot position open
+        new JoystickButton(m_operatorController, Button.kStart.value)
+            .onTrue(new CmdHybridManip_PivotGoToPosition(m_pivot, 3.6));
+
+        // Intake spin out
+        new JoystickButton(m_operatorController, Button.kRightBumper.value) 
+            .onTrue(new CmdHybridManipTimed_Outtake(m_claw, 0.75));
+
+        // Intake spin in
+        new JoystickButton(m_operatorController, Button.kLeftBumper.value)
+            .whileHeld(new CmdHybridManip_Intake(m_claw));
+
+        // Claw switch state of solenoid
+        new JoystickButton(m_operatorController, Button.kRightStick.value) // FLIP PNEUMATICS STATE
+            .onTrue(new InstantCommand(()-> m_reach.use()));
+    }
+
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand() {
+        return new SequentialCommandGroup(
+            new ParallelCommandGroup(
+                new CmdHybridManip_ElevatorGoToPosition(m_elevator, Constants.ElevatorSubsystemConstants.kTopLayerHeight),
+                new CmdHybridManip_PivotGoToPosition(m_pivot, 2.3)
+            ),
+            new CmdHybridManipTimed_Outtake(m_claw, 0.6),
+            new ParallelCommandGroup(
+                new CmdAutonDrive_LeaveCommZone(m_robotDrive),
+                new CmdHybridManip_PivotGoToPosition(m_pivot, 0),
+                new CmdHybridManip_ElevatorGoToPosition(m_elevator, 0)
+            )
+
+        );
+    }
+
+    /**
+     * Sets the initial position and orientation of the robot based on the alliance start position.
+     *
+     * @param xPosition  the x-coordinate of the robot's starting position
+     * @param yPosition  the y-coordinate of the robot's starting position
+     * @param orientation  the orientation of the robot, represented as an integer where 0 represents 0 degrees,
+     *                     1 represents 90 degrees, 2 represents 180 degrees, and 3 represents 270 degrees.
+     */
+    public void setInitialAlliancePosition(double xPosition, double yPosition, int orientation) {
+        m_robotDrive.resetOdometry(new Pose2d(xPosition, yPosition, new Rotation2d(orientation * 2 * Math.PI)));
+    }
+}
